76230b14c2d9b91d6f5e2016cc61a6109d51181c
John A. De Goes <john@degoes.net>
2014-03-21 14:22:03 -0600

    ongoing work to generic tree traversal

--- src/main/scala/slamdata/engine/analysis/term.scala
+++ src/main/scala/slamdata/engine/analysis/term.scala
@@ -1,13 +1,16 @@
 package slamdata.engine
 
-import scalaz.{Functor, Cofree, Foldable, Show, Cord, Tree => ZTree, Monad, Traverse}
+import scalaz.{Functor, Cofree, Foldable, Show, Cord, Tree => ZTree, Monad, Traverse, Free, Arrow, Kleisli}
 
 import scalaz.Tags.Disjunction
 
 import scalaz.std.anyVal._
 import scalaz.std.list._
+import scalaz.std.option._
+import scalaz.std.function._
 
 import scalaz.syntax.monad._
+
 
 final case class Term[F[_]](unFix: F[Term[F]]) {
   def cofree(implicit f: Functor[F]): Cofree[F, Unit] = {
@@ -29,8 +32,8 @@ final case class Term[F[_]](unFix: F[Term[F]]) {
     } yield desc
   }
 
-  def transform(f: Term[F] => Term[F])(implicit F: Functor[F]): Term[F] = {
-    Term(F.map(unFix)(_ transform f))
+  def transform(f: Term[F] => Term[F])(implicit T: Traverse[F]): Term[F] = {
+    transformM[Free.Trampoline]((v: Term[F]) => f(v).pure[Free.Trampoline]).run
   }
 
   def transformM[M[_]](f: Term[F] => M[Term[F]])(implicit M: Monad[M], TraverseF: Traverse[F]): M[Term[F]] = {
@@ -44,8 +47,8 @@ final case class Term[F[_]](unFix: F[Term[F]]) {
     loop(this)    
   }
 
-  def topDownTransform(f: Term[F] => Term[F])(implicit F: Functor[F]): Term[F] = {
-    Term[F](F.map(unFix)(f).map(_ topDownTransform f))
+  def topDownTransform(f: Term[F] => Term[F])(implicit T: Traverse[F]): Term[F] = {
+    topDownTransformM[Free.Trampoline]((term: Term[F]) => f(term).pure[Free.Trampoline]).run
   }
 
   def topDownTransformM[M[_]](f: Term[F] => M[Term[F]])(implicit M: Monad[M], TraverseF: Traverse[F]): M[Term[F]] = {
@@ -66,6 +69,43 @@ final case class Term[F[_]](unFix: F[Term[F]]) {
   def descendM[M[_]](f: Term[F] => M[Term[F]])(implicit M: Monad[M], TraverseF: Traverse[F]): M[Term[F]] = {
     TraverseF.traverse(unFix)(f).map(Term.apply _)
   }
+
+  def rewrite(f: Term[F] => Option[Term[F]])(implicit T: Traverse[F]): Term[F] = {
+    rewriteM[Free.Trampoline]((term: Term[F]) => f(term).pure[Free.Trampoline]).run
+  }
+
+  def rewriteM[M[_]](f: Term[F] => M[Option[Term[F]]])(implicit M: Monad[M], TraverseF: Traverse[F]): M[Term[F]] = {
+    transformM[M] { term =>
+      for {
+        x <- f(term)
+        y <- Traverse[Option].traverse(x)(_ rewriteM f).map(_.getOrElse(term))
+      } yield y
+    }
+  }
+
+  def restructure[G[_]](f: F[Term[G]] => G[Term[G]])(implicit T: Traverse[F]): Term[G] = {
+    restructureM[Free.Trampoline, G]((term: F[Term[G]]) => f(term).pure[Free.Trampoline]).run
+  }
+
+  def restructureM[M[_], G[_]](f: F[Term[G]] => M[G[Term[G]]])(implicit M: Monad[M], T: Traverse[F]): M[Term[G]] = {
+    for {
+      x <- T.traverse(unFix)(_ restructureM f)
+      y <- f(x)
+    } yield Term(y)
+  }
+
+  import Term._
+
+  def context(implicit T: Traverse[F]): Attr[F, Term[F] => Term[F]] = {
+    def loop(f: Term[F] => Term[F]): Attr[F, Term[F] => Term[F]] = {
+      //def g(y: Term[F], replace: Term[F] => Term[F]) = loop()
+
+      ???
+    }
+
+    loop(identity[Term[F]])
+  }
+
 }
 
 trait TermDemo {
@@ -76,6 +116,31 @@ trait TermDemo {
   def node(v: Node[Term[Node]]): Term[Node] = Term[Node](v)
 
   node(Add(node(Int()), node(Int()))) 
+}
+
+trait Holes {
+  case object Hole
+
+  def holes[F[_]: Traverse, A](fa: F[A]): F[(A, A => F[A])] = {
+    (Traverse[F].mapAccumL(fa, 0) {
+      case (i, x) =>
+        val h: A => F[A] = { y =>
+          val g: (Int, A) => (Int, A) = (j, z) => (j + 1, if (i == j) y else z)
+
+          Traverse[F].mapAccumL(fa, 0)(g)._2
+        }
+
+        (i + 1, (x, h))
+    })._2
+  }
+
+  def holesList[F[_]: Traverse, A](fa: F[A]): List[(A, A => F[A])] = Traverse[F].toList(holes(fa))
+
+  def apply[F[_]: Traverse, A](fa: F[A])(f: A => A): F[F[A]] = {
+    val g: (A, A => F[A]) => F[A] = (x, replace) => replace(f(x))
+
+    Traverse[F].map(holes(fa))(g.tupled)
+  }
 }
 
 object Term {
@@ -102,7 +167,9 @@ object Term {
 
   def forget[F[_], A](attr: Attr[F, A])(implicit F: Functor[F]): Term[F] = Term(F.map(attr.unFix.unAnn)(forget[F, A](_)))
 
-  case object Hole
+
+
+
 
   implicit def TermShow[F[_]](implicit showF: Show[F[Term[F]]], foldF: Foldable[F]) = new Show[Term[F]] {
     override def show(term: Term[F]): Cord = {
