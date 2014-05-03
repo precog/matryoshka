c2076103594017cd3797c1f52ffe19bac4577f47
John A. De Goes <john@degoes.net>
2014-05-02 20:12:20 -0600

    added first test of compiler using new custom matcher

--- src/test/scala/slamdata/engine/compiler.scala
+++ src/test/scala/slamdata/engine/compiler.scala
@@ -3,12 +3,15 @@ package slamdata.engine
 import slamdata.engine.analysis.fixplate._
 import slamdata.engine.analysis._
 import slamdata.engine.sql.SQLParser
+import slamdata.engine.std._
 
 import scalaz._
 
 import org.specs2.mutable._
+import org.specs2.matcher.{Matcher, Expectable}
 
 class CompilerSpec extends Specification {
+  import StdLib.structural._
   import LogicalPlan._
   import SemanticAnalysis._
 
@@ -20,15 +23,28 @@ class CompilerSpec extends Specification {
     } yield cld
   }
 
-  val equal = Equal[Term[LogicalPlan]].equal _
+  case class equalPlan(expected: Term[LogicalPlan]) extends Matcher[Term[LogicalPlan]] {
+    val equal = Equal[Term[LogicalPlan]].equal _
 
-  def test(query: String, expected: Term[LogicalPlan]) = {
-    c(query).map(actual => equal(expected, actual)) must beSome(true)
+    def apply[S <: Term[LogicalPlan]](s: Expectable[S]) = {
+      result(equal(expected, s.value),
+             s.description + " is not equal to " + expected,
+             s.description + " is unexpectedly equal to " + expected,
+             s)
+    }
   }
 
-  "select constant" should {
-    "compile" in {
-      true must beTrue
+  def succeed(query: String, expected: Term[LogicalPlan]) = {
+    c(query) must beSome(equalPlan(expected))
+  }
+
+  "compiler" should {
+    "compile select 1" in {
+      succeed("select 1", 
+        Term(
+          Invoke(MakeObject, List(constant(Data.Str("0")), constant(Data.Int(1))))
+        )
+      )
     }
   }
 }
