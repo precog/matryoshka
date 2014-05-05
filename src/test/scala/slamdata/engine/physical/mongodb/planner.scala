c23d143bcbccfb6750e4151f4298aa0beaeaea84
John A. De Goes <john@degoes.net>
2014-05-05 11:51:48 -0600

    added spec scaffolding

--- src/test/scala/slamdata/engine/physical/mongodb/planner.scala
+++ src/test/scala/slamdata/engine/physical/mongodb/planner.scala
@@ -1,2 +1,20 @@
 package slamdata.engine.physical.mongodb
 
+import slamdata.engine._
+import slamdata.engine.analysis.fixplate._
+import slamdata.engine.analysis._
+import slamdata.engine.sql.SQLParser
+import slamdata.engine.std._
+
+import scalaz._
+
+import org.specs2.mutable._
+import org.specs2.matcher.{Matcher, Expectable}
+
+class PlannerSpec extends Specification {
+  import StdLib._
+  import structural._
+  import math._
+  import LogicalPlan._
+  import SemanticAnalysis._
+}
