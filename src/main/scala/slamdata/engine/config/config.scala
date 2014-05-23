fbed9112e0534c27ca7c4187d71e978dc8540d47
John A. De Goes <john@degoes.net>
2014-05-23 14:50:32 -0600

    add config methods to load from file or parse from string

--- src/main/scala/slamdata/engine/config/config.scala
+++ src/main/scala/slamdata/engine/config/config.scala
@@ -2,6 +2,10 @@ package slamdata.engine.config
 
 import argonaut._, Argonaut._
 
+import scalaz.concurrent.Task
+
+import scalaz.\/
+
 sealed trait BackendConfig
 
 final case class MongoDbConfig(server: String, port: Option[Int], username: Option[String], password: Option[String]) extends BackendConfig
@@ -24,4 +28,18 @@ final case class Config(
 
 object Config {
   implicit def Codec = casecodec1(Config.apply, Config.unapply)("mountings")
+
+  def fromFile(path: String): Task[Config] = Task.delay {
+    import java.nio.file._
+    import java.nio.charset._
+
+    val text = new String(Files.readAllBytes(Paths.get("file")), StandardCharsets.UTF_8);
+
+    fromString(text).fold(
+      error => throw new RuntimeException(error),
+      identity
+    )
+  }
+
+  def fromString(value: String): String \/ Config = Parse.decodeEither[Config](value)
 }
