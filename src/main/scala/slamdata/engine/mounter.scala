7f133848e9362fce6a5a8172dc2dc326cae4a43d
John A. De Goes <john@degoes.net>
2014-05-27 16:37:28 -0600

    code cleanup

--- src/main/scala/slamdata/engine/mounter.scala
+++ src/main/scala/slamdata/engine/mounter.scala
@@ -8,17 +8,17 @@ import Scalaz._
 import scalaz.concurrent._
 
 object Mounter {
-  sealed trait MounterError extends Error
-  case class MissingDataSource(path: String, config: BackendConfig) extends MounterError {
+  sealed trait MountError extends Error
+  case class MissingDataSource(path: String, config: BackendConfig) extends MountError {
     def message = "No data source could be mounted at the path " + path + " using the config " + config
   }
 
-  def mountE(config: Config): MounterError \/ Task[Map[String, Backend]] = {
+  def mountE(config: Config): MountError \/ Task[Map[String, Backend]] = {
     type MapString[X] = Map[String, X]
-    type EitherError[X] = MounterError \/ X
+    type EitherError[X] = MountError \/ X
 
-    val map: MounterError \/ Map[String, Task[Backend]] = Traverse[MapString].sequence[EitherError, Task[Backend]](config.mountings.transform {
-      case (path, config) => BackendDefinitions.All(config).map(backend => \/-(backend)).getOrElse(-\/(MissingDataSource(path, config): MounterError))
+    val map: MountError \/ Map[String, Task[Backend]] = Traverse[MapString].sequence[EitherError, Task[Backend]](config.mountings.transform {
+      case (path, config) => BackendDefinitions.All(config).map(backend => \/-(backend)).getOrElse(-\/(MissingDataSource(path, config): MountError))
     })
 
     map.map { map =>
