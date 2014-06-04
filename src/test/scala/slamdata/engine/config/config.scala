af92313031273e168b1b8c1be49f7a38699f1fce
John A. De Goes <john@degoes.net>
2014-06-04 09:39:33 -0600

    add spec for config

---
+++ src/test/scala/slamdata/engine/config/config.scala
@@ -0,0 +1,35 @@
+package slamdata.engine.config
+
+import org.specs2.mutable._
+
+class ConfigSpec extends Specification {
+  val TestConfig = Config(
+    server = SDServerConfig(Some(92)),
+    mountings = Map(
+      "/" -> MongoDbConfig("slamengine-test-01", "mongodb://slamengine:slamengine@ds045089.mongolab.com:45089/slamengine-test-01")
+    )
+  )
+  
+  // TODO: Add ScalaCheck to verify round-trippability of serialization
+
+  "fromString" should {
+    "parse valid config" in {
+      Config.fromString("""
+      {
+        "server": {
+          "port": 92
+        },
+
+        "mountings": {
+          "/": {
+            "mongodb": {
+              "database": "slamengine-test-01",
+              "connectionUri": "mongodb://slamengine:slamengine@ds045089.mongolab.com:45089/slamengine-test-01"
+            }
+          }
+        }
+      }
+      """).toOption must beSome(TestConfig)
+    }
+  }
+}
