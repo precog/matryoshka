0b05bea32fb55e728fae0b96b86f2f755cb33be0
Moss Prescott <moss@theprescotts.com>
2014-06-10 16:54:24 -0600

    #25: tests for simplify

--- src/test/scala/slamdata/engine/compiler.scala
+++ src/test/scala/slamdata/engine/compiler.scala
@@ -438,7 +438,7 @@ class CompilerSpec extends Specification with CompilerHelpers {
           )
         )
       )
-    }//.pendingUntilFixed
+    }
 
     "compile simple sum" in {
       testLogicalPlanCompile(
@@ -458,6 +458,62 @@ class CompilerSpec extends Specification with CompilerHelpers {
       )
     }
 
+  }
+  
+  "simplify" should {
+    
+    "inline trivial binding" in {
+      val lp = letOne('tmp0, read("foo"), free('tmp0))
+      
+      Optimizer.simplify(lp) should_== read("foo")
+    }
+    
+    "not inline binding that's used twice" in {
+      val lp = letOne('tmp0, 
+                      read("foo"), 
+                      makeObj(
+                        "bar" -> ObjectProject(free('tmp0), constant(Data.Str("bar"))),
+                        "baz" -> ObjectProject(free('tmp0), constant(Data.Str("baz")))
+                      )
+                    )
+      
+      Optimizer.simplify(lp) should_== lp
+    }
+    
+    "partially inline a more interesting case" in {
+      val lp = letOne('tmp0,
+                  read("person"),
+                    letOne('tmp1,
+                      makeObj(
+                        "name" -> ObjectProject(free('tmp0), constant(Data.Str("name")))
+                      ),
+                      letOne('tmp2,
+                        OrderBy(
+                          free('tmp1),
+                          MakeArray(
+                            ObjectProject(free('tmp1), constant(Data.Str("name")))
+                          )
+                        ),
+                        free('tmp2)
+                      )
+                    )
+                  )
+                  
+      val slp = letOne('tmp1,
+                  makeObj(
+                    "name" -> ObjectProject(read("person"), constant(Data.Str("name")))
+                  ),
+                  OrderBy(
+                    free('tmp1),
+                    MakeArray(
+                      ObjectProject(free('tmp1), constant(Data.Str("name")))
+                    )
+                  )
+                )
+              
+      Optimizer.simplify(lp) should_== slp
+    }
+    
   }
   
 }
