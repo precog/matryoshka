e223b7467357a2a22804d9e37c1f1d9e70602ee8
Moss Prescott <moss@theprescotts.com>
2014-06-18 12:15:40 -0600

    use Show when there is an expected value

--- src/test/scala/slamdata/engine/matchers.scala
+++ src/test/scala/slamdata/engine/matchers.scala
@@ -80,27 +80,32 @@ trait DisjunctionMatchers {
     }
   } 
 
-  def beLeftDisj[A, B](p: A => Boolean): Matcher[A \/ B] = new Matcher[A \/ B] {
+  def beLeftDisj[A, B](p: A => Boolean)(implicit sa: Show[A]): Matcher[A \/ B] = new Matcher[A \/ B] {
     def apply[S <: A \/ B](s: Expectable[S]) = {
       val v = s.value
+      val vs = v.fold(a => sa.show(a), b => b.toString)
 
-      result(v.fold(p, _ => true), s"$v is left", s"$v is not left", s)
+      result(v.fold(p, _ => true), s"$vs is left", s"$vs is not left", s)
     }
   }
 
-  def beRightDisj[A, B](expected: B): Matcher[A \/ B] = new Matcher[A \/ B] {
+  def beRightDisj[A, B](expected: B)(implicit sb: Show[B]): Matcher[A \/ B] = new Matcher[A \/ B] {
     def apply[S <: A \/ B](s: Expectable[S]) = {
       val v = s.value
-
-      result(v.fold(_ => false, _ == expected), s"$v is right $expected", s"$v is not right $expected", s)
+      val vs = v.fold(a => a.toString(), b => sb.show(b))
+      val exps = sb.show(expected)
+      
+      result(v.fold(_ => false, _ == expected), s"$vs is right $exps", s"$vs is not right $exps", s)
     }
   } 
 
-  def beLeftDisj[A, B](expected: A): Matcher[A \/ B] = new Matcher[A \/ B] {
+  def beLeftDisj[A, B](expected: A)(implicit sa: Show[A]): Matcher[A \/ B] = new Matcher[A \/ B] {
     def apply[S <: A \/ B](s: Expectable[S]) = {
       val v = s.value
+      val vs = v.fold(a => sa.show(a), b => b.toString)
+      val exps = sa.show(expected)
 
-      result(v.fold(_ == expected, _ => false), s"$v is left $expected", s"$v is not left $expected", s)
+      result(v.fold(_ == expected, _ => false), s"$vs is left $exps", s"$vs is not left $exps", s)
     }
   }
 }
