b44c50032827ee66b730cecaf651c377fceed484
Moss Prescott <moss@theprescotts.com>
2014-07-08 14:19:03 -0600

    delete file again

--- src/main/scala/test/Test4.scala
+++
@@ -1,8 +0,0 @@
-package test
-import slamdata.engine.BackendDefinitions._
-
-object Test4 extends App {
-  val q = "select city, pop from zips where pop between 10 and 15"
-  
-  println(MongoDB.eval(Query(q), Path("tmp")))
-}
