a6a7bb182beb54c3a3c937459b6cf4a85c53aee9
Moss Prescott <moss@theprescotts.com>
2014-11-18 09:53:02 -0700

    add timestamp, date, time, and interval to the Arbitrary for Node

--- src/test/scala/slamdata/engine/sql/SqlParserSpec.scala
+++ src/test/scala/slamdata/engine/sql/SqlParserSpec.scala
@@ -195,6 +195,8 @@ class SQLParserSpec extends Specification with ScalaCheck with DisjunctionMatche
   
   import org.scalacheck._
   import Gen._
+  import org.threeten.bp.{Duration,Instant}
+  import slamdata.engine.sql._
   
   implicit def arbitraryNode: Arbitrary[Node] = Arbitrary { selectGen(4) }
   
@@ -268,7 +270,11 @@ class SQLParserSpec extends Specification with ScalaCheck with DisjunctionMatche
       } yield SetLiteral(cs)),
       10 -> (for {
         n <- Gen.alphaChar.map(_.toString)  // TODO: generate names requiring quotes, etc.
-      } yield Ident(n))
+      } yield Ident(n)),
+      1 -> Unop(StringLiteral(Instant.now.toString), ToTimestamp),
+      1 -> Gen.choose(0L, 10000000000L).map(millis => Unop(StringLiteral(Duration.ofMillis(millis).toString), ToInterval)),
+      1 -> Unop(StringLiteral("2014-11-17"), ToDate),
+      1 -> Unop(StringLiteral("12:00:00"), ToTime)
     )
 
   def complexExprGen(depth: Int): Gen[Expr] =
