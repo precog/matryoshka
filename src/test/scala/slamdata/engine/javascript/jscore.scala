d520cac179827dd1ef08a14db41b46f5a444f6e7
Moss Prescott <moss@theprescotts.com>
2014-11-26 21:02:39 -0700

    test for `compose`
    Is this the right semantics? Can’t decide for sure, but I think this is
    actually `>>>`/`andThen`.

--- src/test/scala/slamdata/engine/javascript/jscore.scala
+++ src/test/scala/slamdata/engine/javascript/jscore.scala
@@ -23,4 +23,16 @@ class JsCoreSpecs extends Specification with TreeMatchers {
           List(Js.Num(1, false)))
     }
   }
+  
+  "compose" should {
+    "do _.foo, then _.bar" in {
+      val x = JsCore.Ident("x").fix
+
+      val a = JsMacro(JsCore.Select(_, "foo").fix)
+      val b = JsMacro(JsCore.Select(_, "bar").fix)
+      
+      (a compose b)(x).toJs.render(0) must_==
+        "x.foo.bar"
+    }
+  }
 }
