a9abfba6faa4e81ed711de79935756175496ef7a
John A. De Goes <john@degoes.net>
2014-12-08 12:37:04 -0700

    no need to replicate work in jenkinsSuccess

--- scripts/jenkinsSuccess.sh
+++ scripts/jenkinsSuccess.sh
@@ -2,8 +2,4 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [ "$TRAVIS_BUILD" = "true" ] ; then
-  "$DIR/../sbt" coverageAggregate coveralls
-else
-  "$DIR/../sbt" coverageAggregate
-fi
+SBT="$DIR/../sbt"
