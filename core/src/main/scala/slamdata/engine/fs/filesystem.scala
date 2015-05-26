47a22e234b7cc0bf3c3cfcd99a7bac162e7d6194
Greg Pfeil <greg@technomadic.org>
2015-05-26 13:19:45 -0600

    Add `fs.replace()`.
    Including a default impl that is basically `delete; save` and filesystem tests.

--- core/src/main/scala/slamdata/engine/fs/filesystem.scala
+++ core/src/main/scala/slamdata/engine/fs/filesystem.scala
@@ -7,6 +7,7 @@ import scalaz.stream._
 import argonaut._, Argonaut._
 
 import slamdata.engine.{Data, DataCodec}
+import slamdata.engine.fp._
 
 final case class WriteError(value: Data, hint: Option[String]) extends slamdata.engine.Error {
   def message = hint.getOrElse("error writing data") + "; value: " + value
@@ -45,6 +46,10 @@ trait FileSystem {
   def move(src: Path, dst: Path): Task[Unit]
 
   def delete(path: Path): Task[Unit]
+
+  /** Like save, but will replace a file if it already exists. */
+  def replace(path: Path, values: Process[Task, Data]) =
+    delete(path).flatMap(κ(save(path, values)))
 
   def ls(dir: Path): Task[List[Path]]
 
