5e9496f1259251a9084e2c2ea52a07840e9f572f
Emrys Ingersoll <ingersoll@gmail.com>
2015-09-16 11:48:18 -0500

    Update doc style, elminate a messy for-comprehension

--- it/src/test/scala/quasar/backendtest.scala
+++ it/src/test/scala/quasar/backendtest.scala
@@ -16,17 +16,15 @@ import quasar.fs._
 
 object TestConfig {
 
-  /**
-   * The path prefix under which test data may be found as well as where tests
-   * can write test data/output.
-   */
+  /** The path prefix under which test data may be found as well as where tests
+    * can write test data/output.
+    */
   val DefaultTestPathPrefix = Path("/quasar-test/")
 
-  /**
-   * The environment variable used to externally specify the test path prefix.
-   *
-   * NB: The same path prefix is used for all backends under test.
-   */
+  /** The environment variable used to externally specify the test path prefix.
+    *
+    * NB: The same path prefix is used for all backends under test.
+    */
   val TestPathPrefixEnvName = "QUASAR_TEST_PATH_PREFIX"
 
   lazy val backendNames: List[String] = List("mongodb_2_6", "mongodb_3_0")
@@ -40,25 +38,23 @@ object TestConfig {
     Task.delay(System.getenv).liftM[OptionT]
       .flatMap(env => OptionT(Task.delay(Option(env.get(name)))))
 
-  /**
-   * Load backend config from environment variable.
-   * Fails if it cannot parse the config
-   * Returns None if there is no config.
-   */
+  /** Load backend config from environment variable.
+    *
+    * Fails if it cannot parse the config and returns None if there is no config.
+    */
   def loadConfig(name: String): OptionT[Task, BackendConfig] =
     readEnv(backendEnvName(name)).flatMapF(value =>
       Parse.decodeEither[BackendConfig](value).fold(
         e => fail("Failed to parse $" + backendEnvName(name) + ": " + e),
         _.point[Task]))
 
-  /**
-   * Returns the absolute path within a backend to the directory containing test
-   * data.
-   *
-   * One may specify this externally by setting the [[TestPathPrefixEnvName]].
-   * The returned [[Task]] will fail if an invalid path is provided from the
-   * environment and return the [[DefaultTestPathPrefix]] if nothing is provided.
-   */
+  /** Returns the absolute path within a backend to the directory containing test
+    * data.
+    *
+    * One may specify this externally by setting the [[TestPathPrefixEnvName]].
+    * The returned [[Task]] will fail if an invalid path is provided from the
+    * environment and return the [[DefaultTestPathPrefix]] if nothing is provided.
+    */
   def testDataPathPrefix: Task[Path] =
     readEnv(TestPathPrefixEnvName).map(Path(_)).flatMapF { path =>
       if (path.absolute) Task.now(path)
@@ -95,25 +91,22 @@ trait BackendTest extends Specification {
 
   def genTempDir: Task[Path] = genTempFile.map(_.asDir)
 
-  /**
-   * Run the supplied tests against all backends.
-   *
-   * If the fixture fails to load any backends for some reason, an exception is
-   * thrown stating so.
-   *
-   * @param runTests A function that accepts a path prefix under which both test
-   *                 data may be found and tests may use as a sandbox, a backend
-   *                 (filesystem) and an associated name for purposes of
-   *                 identifying the backend in messages.
-   */
+  /** Run the supplied tests against all backends.
+    *
+    * If the fixture fails to load any backends for some reason, an exception is
+    * thrown stating so.
+    *
+    * @param runTests A function that accepts a path prefix under which both test
+    *                 data may be found and tests may use as a sandbox, a backend
+    *                 (filesystem) and an associated name for purposes of
+    *                 identifying the backend in messages.
+    */
   def backendShould(runTests: (Path, Backend, String) => Unit): Unit =
-    (for {
-      dir   <- TestConfig.testDataPathPrefix
-      bknds <- AllBackends
-      _     <- bknds.traverse_ { case (n, b) =>
-                 Task.delay(n should runTests(dir, b, n)).void
-               }
-    } yield ()).run
+    (TestConfig.testDataPathPrefix |@| AllBackends)((dir, backends) =>
+      backends.traverse_ { case (n, b) =>
+        Task.delay(n should runTests(dir, b, n)).void
+      }
+    ).join.run
 
   def deleteTempFiles(backend: Backend, dir: Path): Task[Unit] =
     backend.ls(dir).run.flatMap(_.fold(
