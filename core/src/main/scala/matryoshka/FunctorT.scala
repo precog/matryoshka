2fb3a5b2b4799b3e77fd90a5e9b2f1db090c21a0
Greg Pfeil <greg@technomadic.org>
2016-03-13 22:06:10 -0600

    Build changes.
    Remove CustomKeys.scala and update the copyright.

--- core/src/main/scala/matryoshka/FunctorT.scala
+++ core/src/main/scala/matryoshka/FunctorT.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 - 2015 SlamData Inc.
+ * Copyright 2014–2016 SlamData Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
