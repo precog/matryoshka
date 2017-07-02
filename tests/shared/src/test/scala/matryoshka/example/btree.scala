807393206e0e50ef9bd6c0af27a9266a80594c7b
zainab-ali <zainab.ali.london@gmail.com>
2017-07-02 15:26:56 +0100

    Add some examples

---
+++ tests/shared/src/test/scala/matryoshka/example/btree.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2014–2017 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package matryoshka.example
+
+
+import org.specs2.mutable._
+
+import slamdata.Predef._
+import matryoshka._
+import matryoshka.implicits._
+import scalaz._
+
+sealed trait BTree[A]
+
+object BTree {
+
+  case class BTNil[A]() extends BTree[A]
+  case class Leaf[A](value: Int) extends BTree[A]
+  case class Node[A](l: A , r: A) extends BTree[A]
+
+  implicit val treeFunctor: Functor[BTree] = new Functor[BTree] {
+    def map[A, B](tree: BTree[A])(f: A => B): BTree[B] = tree match {
+      case BTNil() => BTNil()
+      case Leaf(v) => Leaf(v)
+      case Node(l, r) => Node(f(l), f(r))
+    }
+  }
+
+  def merge(l: List[Int], r: List[Int]): List[Int] =
+    (l, r) match {
+	  case(l, Nil) => l
+	  case(Nil, r) => r
+	  case(lh :: ls, rh :: rs) =>
+	    if (lh < rh) lh::merge(ls, r)
+            else rh :: merge(l, rs)
+    }
+
+  //builds a balanced binary tree
+  val to: Coalgebra[BTree, List[Int]] = {
+    case Nil => BTNil()
+    case x :: Nil => Leaf(x)
+    case xs => val (l, r) = xs.splitAt(xs.length / 2)
+      Node(l, r)
+  }
+
+  //sorts a balanced binary tree
+  val mergeSort: Algebra[BTree, List[Int]] = {
+    case BTNil() => Nil
+    case Leaf(v) => v :: Nil
+    case Node(l, r) => merge(l, r)
+  }
+}
+
+class BTreeSpec extends Specification {
+  import BTree._
+
+  "should sort a list" >> {
+    val list = List(23, 565, 6, 23, 45, 25, 678, 5)
+    val sorted = list.hylo(mergeSort, to)
+    sorted should ===(list.sorted)
+  }
+}
---
+++ tests/shared/src/test/scala/matryoshka/example/intList.scala
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2014–2017 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package matryoshka.example
+
+import org.specs2.mutable._
+
+import slamdata.Predef._
+import matryoshka._
+import matryoshka.data._
+import matryoshka.implicits._
+import scalaz._
+
+sealed trait IntList[A]
+
+object IntList {
+
+  case class IntCons[A](h: Int, tail: A) extends IntList[A]
+  case class IntNil[A]() extends IntList[A]
+
+  implicit val intListFunctor: Functor[IntList] = new Functor[IntList] {
+    def map[A, B](list: IntList[A])(f: A => B): IntList[B] = list match {
+      case IntNil() => IntNil()
+      case IntCons(h, t) => IntCons(h, f(t))
+    }
+  }
+
+  val to: Coalgebra[IntList, List[Int]] = {
+    case Nil => IntNil()
+    case h :: t => IntCons(h, t)
+  }
+
+  val from: Algebra[IntList, List[Int]] = {
+    case IntNil() => Nil
+    case IntCons(h, t) => h :: t
+  }
+
+  val sum: Algebra[IntList, Int] = {
+    case IntNil() => 0
+    case IntCons(h, t) => h + t
+  }
+
+  val len: Algebra[IntList, Int] = {
+    case IntNil() => 0
+    case IntCons(_, t) => t + 1
+  }
+
+  def filter(f: Int => Boolean): Algebra[IntList, List[Int]] = {
+    case IntNil() => Nil
+    case IntCons(h, t) => if(f(h)) h :: t else t
+  }
+
+  def lessThan(i: Int): IntList ~> IntList = new (IntList ~> IntList) {
+    def apply[A](l: IntList[A]): IntList[A] = l match {
+      case IntNil() =>
+        println(s"intnil")
+        IntNil()
+      case l @ IntCons(h, t) =>
+        println(s"head is $h")
+        if(h < i) l else IntNil()
+    }
+  }
+
+  def mapHead(f: Int => Int): GCoalgebra[Fix[IntList] \/ ?, IntList, Fix[IntList]] = {
+    case Fix(IntNil()) => IntNil()
+    case Fix(IntCons(h, t)) => IntCons(f(h), \/.left(t))
+  }
+
+  val infinite: Coalgebra[IntList, Int] = n => IntCons(n, n + 1)
+}
+
+class IntListSpec extends Specification {
+  import IntList._
+
+  val intList = Fix(IntCons(1, Fix(IntCons(2, Fix(IntNil[Fix[IntList]]())))))
+
+  "construct an IntList" >> {
+    List(1, 2).ana[Fix[IntList]](to) should ===(intList)
+  }
+  "convert an intlist to a list" >> {
+    intList.cata(from) should ===(List(1, 2))
+  }
+
+  "filter an intlist" >> {
+    (0 until 10).toList.hylo(filter(_ < 5), to) should ===((0 until 5).toList)
+  }
+
+  "map the head of a list" >> {
+    intList.apo.apply(mapHead(_ * 3)).cata(from) should ===(List(3, 2))
+  }
+
+  // "short circuit the creation of infinite intlist" >> {
+  //   1.postpro[Fix[IntList]](IntList.lessThan(10), infinite).cata(len) should ===(9)
+  // }
+
+  "short circuit the fold of a list" >> {
+    intList.prepro(IntList.lessThan(2), sum) should ===(1)
+  }
+}
