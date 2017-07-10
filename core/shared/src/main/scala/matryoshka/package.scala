785e441a78adb93b990f177f961624845601f6bf
Emrys Ingersoll <ingersoll@gmail.com>
2017-07-10 11:54:59 -0500

    Generalize attributeElgot[M] to transform to EnvT.

--- core/shared/src/main/scala/matryoshka/package.scala
+++ core/shared/src/main/scala/matryoshka/package.scala
@@ -104,6 +104,18 @@ package object matryoshka {
     */
   type AlgebraicGTransform[W[_], T, F[_], G[_]]        = F[W[T]] => G[T]
 
+  /** Transform a structure `F` contained in `W`, to a structure `G`,
+    * in bottom-up fashion.
+    * @group algebras
+    */
+  type ElgotAlgebraicTransformM[W[_], M[_], T, F[_], G[_]] = W[F[T]] => M[G[T]]
+
+  /** Transform a structure `F` contained in `W`, to a structure `G`,
+    * in bottom-up fashion.
+    * @group algebras
+    */
+  type ElgotAlgebraicTransform[W[_], T, F[_], G[_]] = W[F[T]] => G[T]
+
   /** Transform a structure `F` to a structure `G` containing values in `N`,
     * in top-down fashion, accumulating effects in the monad `M`.
     * @group algebras
@@ -647,12 +659,30 @@ package object matryoshka {
     * @group algtrans
     */
   object attributeElgotM {
-    def apply[W[_], M[_]] = new PartiallyApplied[W, M]
+    def apply[W[_], M[_], T] = new PartiallyApplied[W, M, T]
 
-    final class PartiallyApplied[W[_], M[_]] {
-      def apply[F[_]: Functor, A](f: ElgotAlgebraM[W, M, F, A])(implicit W: Comonad[W], M: Functor[M]):
-          ElgotAlgebraM[W, M, F, Cofree[F, A]] =
-        node => f(node ∘ (_ ∘ (_.head))) ∘ (Cofree(_, node.copoint))
+    final class PartiallyApplied[W[_], M[_], T] {
+      def apply[F[_]: Functor, A]
+        (f: ElgotAlgebraM[W, M, F, A])
+        (implicit W: Comonad[W], M: Functor[M], T: Recursive.Aux[T, EnvT[A, F, ?]])
+          : ElgotAlgebraicTransformM[W, M, T, F, EnvT[A, F, ?]] =
+        node => f(node ∘ (_ ∘ (_.project.ask))) ∘ (a => EnvT((a, node.copoint)))
+    }
+  }
+
+  /**
+    *
+    * @group algtrans
+    */
+  object attributeElgot {
+    def apply[W[_], T] = new PartiallyApplied[W, T]
+
+    final class PartiallyApplied[W[_], T] {
+      def apply[F[_]: Functor, A]
+        (f: ElgotAlgebra[W, F, A])
+        (implicit W: Comonad[W], T: Recursive.Aux[T, EnvT[A, F, ?]])
+          : ElgotAlgebraicTransform[W, T, F, EnvT[A, F, ?]] =
+        attributeElgotM[W, Id, T](f)
     }
   }
 
